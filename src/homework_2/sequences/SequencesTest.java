af02593f8c1162c188fe4b089627d787926b9749
wborovik <w.borovik@mail.ru>
2023-03-11 13:45:26 +0300

    vol-2.1

---
+++ src/homework_2/sequences/SequencesTest.java
@@ -0,0 +1,20 @@
+package homework_2.sequences;
+
+public class SequencesTest {
+    public static void main(String[] args) {
+        Sequences sequences = new SequencesImpl();
+
+        int n = 10;
+
+        sequences.a(n);
+        sequences.b(n);
+        sequences.c(n);
+        sequences.d(n);
+        sequences.e(n);
+        sequences.f(n);
+        sequences.g(n);
+        sequences.h(n);
+        sequences.i(n);
+        sequences.j(n);
+    }
+}
