b98a672dd68e6d616604b908468483bfccfb1dd1
wborovik <w.borovik@mail.ru>
2023-03-11 16:54:50 +0300

    vol-2.2

--- src/homework_2/complex_numbers/ComplexNumberTest.java
+++ src/homework_2/complex_numbers/ComplexNumberTest.java
@@ -4,10 +4,10 @@ public class ComplexNumberTest {
     public static void main(String[] args) {
         System.out.println("Создание комплексных чисел:");
 
-        ComplexNumberImpl complex = new ComplexNumberImpl(7.8);
+        ComplexNumber complex = new ComplexNumber(7.8);
 
-        ComplexNumberImpl complex1 = new ComplexNumberImpl(21.5, 3.5);
-        ComplexNumberImpl complex2 = new ComplexNumberImpl(5.2, 15.2);
+        ComplexNumber complex1 = new ComplexNumber(21.5, 3.5);
+        ComplexNumber complex2 = new ComplexNumber(5.2, 15.2);
 
         System.out.println(complex);
         System.out.println(complex1);
